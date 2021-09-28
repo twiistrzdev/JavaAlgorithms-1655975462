14af610a47bcf6365b858ff5f1eee67c743c7981
daishitie <daishitie@gmail.com>
2021-09-28 19:21:43 +0800

    update comments

--- src/com/daishitie/algorithms/BinarySearch.java
+++ src/com/daishitie/algorithms/BinarySearch.java
@@ -1,23 +1,20 @@
 package com.daishitie.algorithms;
 
 /**
- * # BinarySearch
- *
+ * # Binary Search
  * Binary search is a textbook algorithm based on the idea to compare the target value to the middle element of the array.
  * If the target value is equal to the middle element - we're done.
  * If the target value is smaller - continue to search on the left.
  * If the target value is larger - continue to search on the right.
  *
  * ## Algorithm
- * ```
  * Initialise left and right pointers : left = 0, right = n - 1.
  * While left <= right :
  *   Compare middle element of the array 'nums[pivot]' to the target value target.
  *     If the middle element is the target 'target = nums[pivot]' : return pivot.
- *     If the target is not yet found :
+ *     If the target is not yet found:
  *       If 'target < nums[pivot]', continue the search on the left 'right = pivot - 1'.
  *       Else continue the search on the right 'left = pivot + 1'.
- * ```
  */
 public class BinarySearch {
     public static int search(int[] nums, int target) {
